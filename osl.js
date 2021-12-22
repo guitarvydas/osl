5c3f2e0108061bcc5976bcdb625b4102ca8ae301
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-22 15:31:29 -0500

    remove scopestack

--- osl.js
+++ osl.js
@@ -198,62 +198,7 @@ function transpiler (scnText, grammar, semOperation, semanticsObject, errorMessa
 }
 
 
-var _scope;
-
-function scopeStack () {
-    this._stack = [];
-    this.pushNew = function () {this._stack.push ([])};
-    this.pop = function () {this._stack.pop ()};
-    this._topIndex = function () {return this._stack.length - 1;};
-    this._top = function () { return this._stack[this._topIndex ()]; };
-    this.scopeAdd = function (key, val) {
-	this._top ().push ({key: key, val: val});
-    };
-    this._lookup = function (key, a) { 
-	return a.find (obj => {return obj && obj.key && (obj.key === key)}); };
-    this.scopeGet = function (key) {
-	var i = this._topIndex ();
-	for (; i >= 0 ; i -= 1) {
-	    var obj = this._lookup (key, this._stack [i]);
-	    if (obj) {
-		return obj.val;
-	    };
-	};
-        console.log ('*** scopeGet error ' + key + ' ***');
-	console.log (this._stack);
-	console.log (key);
-	throw "scopeGet internal error - can't find /" + key + "/";
-    };
-    this.scopeModify = function (key, val) {
-	var i = this._topIndex ();
-	for (; i >= 0 ; i -= 1) {
-	    var obj = this._lookup (key, this._stack [i]);
-	    if (obj) {
-              obj.val = val;
-              return val;
-	    };
-	};
-        console.log ('*** scopeModify error ' + key + ' ***');
-	console.log (this._stack);
-	console.log (key);
-	throw "scopeModify internal error " + key;
-    };
-}
-
-function scopeAdd (key, val) {
-  return _scope.scopeAdd (key, val);
-}
-
-function scopeModify (key, val) {
-  return _scope.scopeModify (key, val);
-}
-
-function scopeGet (key) {
-  return _scope.scopeGet (key);
-}
-
 function _ruleInit () {
-    _scope = new scopeStack ();
 }
 
 function traceSpaces () {
@@ -275,7 +220,6 @@ function _ruleEnter (ruleName) {
 	process.stderr.write (ruleName.toString ());
 	process.stderr.write ("\n");
     }
-    _scope.pushNew ();
 }
 
 function _ruleExit (ruleName) {
@@ -286,7 +230,6 @@ function _ruleExit (ruleName) {
 	process.stderr.write (ruleName); 
 	process.stderr.write ("\n");
     }
-    _scope.pop ();
 }
 
 
